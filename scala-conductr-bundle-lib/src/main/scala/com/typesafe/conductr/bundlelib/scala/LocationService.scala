36f7d3bb754fef6693553b22f2e6baadc1bf4d53
Christopher Hunt <huntchr@gmail.com>
2015-02-05 23:38:17 +1100

    LocationService now returns caching information
    ...almost anyhow... there's a FIXME to actually read the Cache-Control header, but the API is of immediate concern.
    
    Documentation is also provided for the scala lib.

--- scala-conductr-bundle-lib/src/main/scala/com/typesafe/conductr/bundlelib/scala/LocationService.scala
+++ scala-conductr-bundle-lib/src/main/scala/com/typesafe/conductr/bundlelib/scala/LocationService.scala
@@ -12,6 +12,7 @@ import com.typesafe.conductr.bundlelib.{ LocationService => JavaLocationService 
 import com.typesafe.conductr.bundlelib.scala.ConnectionHandler.withConnectedRequest
 
 import scala.concurrent._
+import scala.concurrent.duration._
 
 /**
  * LocationService used to look up services using the Typesafe ConductR Service Locator.
@@ -23,14 +24,18 @@ object LocationService {
    * component's endpoint data structure i.e. within a bundle's bundle.conf.
    *
    * Returns some URL representing the service or None if the service is not found or if this
-   * program is not running in the context of ConductR.
+   * program is not running in the context of ConductR. An optional maxAge duration is also returned
+   * indicating that the value may be cached for by the caller up to this period of time.
    */
-  def lookup(serviceName: String)(implicit ec: ExecutionContext): Future[Option[URL]] =
+  def lookup(serviceName: String)(implicit ec: ExecutionContext): Future[Option[(URL, Option[FiniteDuration])]] =
     withConnectedRequest(Option(JavaLocationService.createLookupPayload(serviceName))) { con =>
       con.getResponseCode match {
         case 307 =>
           Option(con.getHeaderField("Location"))
-            .map(new URL(_))
+            .map { location =>
+              val url = new URL(location)
+              url -> None // FIXME: Need to interpret max-age here.
+            }
             .orElse(throw new IOException("Missing Location header"))
         case 404 =>
           None
