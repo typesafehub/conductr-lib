1c9c7239e67782ff4a7b15f8339e9caf99c5f543
Heiko Seeberger <mail@heikoseeberger.de>
2015-04-23 20:42:26 +0200

    Upgrade to Akka 2.3.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,25 +6,25 @@
 import sbt._
 
 object Version {
-  val scala      = "2.11.6"
-  val scalaTest  = "2.2.3"
-  val akka23     = "2.3.9"
+  val akka23     = "2.3.10"
   val akka23Http = "1.0-M5"
+  val junit      = "4.12"
   val play23     = "2.3.8"
   val play24     = "2.4.0-M3"
-  val junit      = "4.12"
+  val scala      = "2.11.6"
+  val scalaTest  = "2.2.3"
 }
 
 object Library {
   val akka23Cluster     = "com.typesafe.akka" %% "akka-cluster"                   % Version.akka23
   val akka23Http        = "com.typesafe.akka" %% "akka-http-experimental"         % Version.akka23Http
-  val akka23Testkit     = "com.typesafe.akka" %% "akka-testkit"                   % Version.akka23
   val akka23HttpTestkit = "com.typesafe.akka" %% "akka-http-testkit-experimental" % Version.akka23Http
+  val akka23Testkit     = "com.typesafe.akka" %% "akka-testkit"                   % Version.akka23
+  val junit             = "junit"             %  "junit"                          % Version.junit
   val play23Test        = "com.typesafe.play" %% "play-test"                      % Version.play23
   val play23Ws          = "com.typesafe.play" %% "play-ws"                        % Version.play23
   val play24Test        = "com.typesafe.play" %% "play-test"                      % Version.play24
   val play24Ws          = "com.typesafe.play" %% "play-ws"                        % Version.play24
-  val junit             = "junit"             %  "junit"                          % Version.junit
   val scalaTest         = "org.scalatest"     %% "scalatest"                      % Version.scalaTest
 }
 
