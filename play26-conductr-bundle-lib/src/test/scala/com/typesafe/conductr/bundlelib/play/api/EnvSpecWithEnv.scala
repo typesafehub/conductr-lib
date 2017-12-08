4f924df2cf55967ba015f8f6c7a6574402efd4cb
Felix Satyaputra <felix.satyaputra@typesafe.com>
2017-12-08 17:30:49 +1100

    Play 2.6 Bundle Lib: append allow hosts
    Obtain the reference to existing config, and append `BUNDLE_HOST_IP` to the list of allowed hosts

--- play26-conductr-bundle-lib/src/test/scala/com/typesafe/conductr/bundlelib/play/api/EnvSpecWithEnv.scala
+++ play26-conductr-bundle-lib/src/test/scala/com/typesafe/conductr/bundlelib/play/api/EnvSpecWithEnv.scala
@@ -1,6 +1,7 @@
 package com.typesafe.conductr.bundlelib.play.api
 
 import com.typesafe.conductr.lib.AkkaUnitTest
+import com.typesafe.config.ConfigFactory
 
 import scala.collection.JavaConverters._
 
@@ -12,6 +13,19 @@ class EnvSpecWithEnv extends AkkaUnitTest("EnvSpecWithEnvForHost") {
     "initialize the env like expected" in {
       config.getString("play.akka.actor-system") shouldBe "somesys-v1"
       config.getStringList("play.filters.hosts.allowed").asScala shouldBe List("1.1.1.1")
+    }
+
+    "merged the allowed hosts config" in {
+      val existing = ConfigFactory.parseString(
+        """
+          |play.filters.hosts.allowed = ["a.com", "b.com"]
+        """.stripMargin
+      )
+
+      val result = Env.asConfig("my-system", existing)
+
+      result.getString("play.akka.actor-system") shouldBe "my-system"
+      result.getStringList("play.filters.hosts.allowed").asScala shouldBe List("a.com", "b.com", "1.1.1.1")
     }
   }
 }
